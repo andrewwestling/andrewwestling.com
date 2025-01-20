7a09ca16f00af818c2a58145aa1581f3ae732d4f
Andrew Westling <a@andrewwestling.com>
2025-01-19 22:42:03 -0800

    Make Upcoming badge a link
    - Link to the Upcoming page, like the BucketList badge links to the BucketList page

--- next-js/app/music/components/Upcoming.tsx
+++ next-js/app/music/components/Upcoming.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import { routes } from "../lib/routes";
+
 export const Upcoming = () => {
   return (
     <span className="bg-primary rounded text-xs text-white px-1 py-0.5 !font-normal whitespace-nowrap">
-      Upcoming
+      <Link className="hover:text-white" href={routes.upcoming()}>
+        Upcoming
+      </Link>
     </span>
   );
 };
