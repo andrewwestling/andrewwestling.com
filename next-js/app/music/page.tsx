13885b7b0331b214317d434fea7b93869d4cddd6
Andrew Westling <a@andrewwestling.com>
2025-01-20 00:46:56 -0800

    Rework homepage
    - Masonry grid thing going on, 2 on top, 3 on bottom
    - The entry point is the hardest page 😐

--- next-js/app/music/page.tsx
+++ next-js/app/music/page.tsx
@@ -2,8 +2,27 @@ import Link from "next/link";
 import { routes } from "./lib/routes";
 
 export default async function HomePage() {
-  const linkClasses =
-    "bg-surface dark:bg-surface-dark p-3 rounded-md cursor-pointer";
+  const linkClasses = [
+    "bg-surface",
+    "dark:bg-surface-dark",
+    "p-3",
+    "py-6",
+    "rounded-md",
+    "cursor-pointer",
+    "flex",
+    "flex-row",
+    "items-center",
+    "justify-center",
+    "shadow-sm",
+    "hover:shadow-md",
+    "dark:hover:border-muted",
+    "border",
+    "border-border",
+    "dark:border-border-dark",
+    "transition-all",
+    "duration-300",
+    "ease-in-out",
+  ].join(" ");
 
   return (
     <div className="grid gap-6">
@@ -15,30 +34,43 @@ export default async function HomePage() {
       </section>
 
       {/* Browse links */}
-      <div className="grid md:grid-cols-2 gap-6">
+      <div className="grid md:grid-cols-6 gap-6">
         {/* Upcoming Concerts */}
         <Link
-          className={`${linkClasses} flex gap-1 no-underline`}
+          className={`${linkClasses} flex gap-1 no-underline md:col-span-3`}
           href={routes.upcoming()}
         >
-          <span className="no-underline">✨</span>
+          <span className="no-underline">📆</span>
           <span className="underline">Upcoming Concerts</span>
         </Link>
+        {/* Bucket List */}
+        <Link
+          className={`${linkClasses} md:col-span-3 flex gap-1 no-underline`}
+          href={routes.bucketList()}
+        >
+          <span className="no-underline">🤞</span>
+          <span className="underline">Bucket List</span>
+        </Link>
         {/* All Seasons */}
-        <Link className={linkClasses} href={routes.seasons.index()}>
+        <Link
+          className={`${linkClasses} md:col-span-2`}
+          href={routes.seasons.index()}
+        >
           All Seasons
         </Link>
         {/* All Concerts */}
-        <Link className={linkClasses} href={routes.concerts.index()}>
+        <Link
+          className={`${linkClasses} md:col-span-2`}
+          href={routes.concerts.index()}
+        >
           All Concerts
         </Link>
         {/* All Works */}
-        <Link className={linkClasses} href={routes.works.index()}>
+        <Link
+          className={`${linkClasses} md:col-span-2`}
+          href={routes.works.index()}
+        >
           All Works
-        </Link>
-        {/* Bucket List */}
-        <Link className={linkClasses} href={routes.bucketList()}>
-          Bucket List
         </Link>
       </div>
     </div>
