02ddb0815fe5b4e9eea28c7bbaed65fee9675883
Andrew Westling <a@andrewwestling.com>
2025-01-26 10:41:31 -0500

    Fix text sizes on mobile
    - Page Titles were too big on phone; makes the text break weird

--- next-js/app/music/components/PageTitle.tsx
+++ next-js/app/music/components/PageTitle.tsx
@@ -4,5 +4,9 @@ interface PageTitleProps {
 }
 
 export function PageTitle({ children, className = "" }: PageTitleProps) {
-  return <h1 className={`text-4xl font-bold ${className}`}>{children}</h1>;
+  return (
+    <h1 className={`text-2xl sm:text-4xl font-bold ${className}`}>
+      {children}
+    </h1>
+  );
 }
