48482564a80db8015583a738acb1c908237e856d
Andrew Westling <a@andrewwestling.com>
2025-01-26 16:33:15 -0500

    Add extra space to bottom of all pages
    - Scrolling needs some padding at the bottom, just adding this on the layout

--- next-js/app/music/layout.tsx
+++ next-js/app/music/layout.tsx
@@ -7,7 +7,7 @@ export default function MusicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 pb-24">
       <Breadcrumbs />
       <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
     </div>
