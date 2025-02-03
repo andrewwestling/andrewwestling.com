694d613345b75def81b7d5843e4c403125660239
Andrew Westling <a@andrewwestling.com>
2025-02-03 17:05:54 -0500

    Update Breadcrumbs example to show scrolling behavior
    - Update ComponentWrapper to support the overflow stuff
    - Update VariantWrapper to support having extra borders added (I'm throwing negative margin-top on when a border class is used so I can stack the label's border on top of the container's border and not have double-border)
    - Rework Breadcrumbs file to make the Homepage nav menu also scroll the menu too; make shared components FixedSection and ScrollSection to facilitate both having the same behavior

--- next-js/app/awds/components/VariantWrapper.tsx
+++ next-js/app/awds/components/VariantWrapper.tsx
@@ -1,10 +1,18 @@
 import { ReactNode } from "react";
 
 export const VariantWrapper = (
-  props: React.PropsWithChildren & { label: string; meta?: ReactNode }
+  props: React.PropsWithChildren & {
+    label: string;
+    meta?: ReactNode;
+    className?: string;
+  }
 ) => (
-  <div className="flex flex-col gap-2">
-    <label className="text-xs rounded border border-fuchsia-500 border-dashed text-fuchsia-500 px-1 w-fit">
+  <div className={`flex flex-col gap-2 min-w-0 ${props.className}`}>
+    <label
+      className={`${
+        props.className?.includes("border-t" || "border") ? "mt-[-1px]" : ""
+      } text-xs rounded border border-fuchsia-500 border-dashed text-fuchsia-500 px-1 w-fit`}
+    >
       {props.label}
     </label>
     {props.meta && (
